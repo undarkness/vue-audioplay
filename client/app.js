d9b1361817e5a58fdeb773c8994ca12462b6968b
undarkness <407645831@qq.com>
2017-05-30 19:20:02 +0800

    改
    问题多多,路由需要重写

--- client/app.js
+++ client/app.js
@@ -1,33 +1,64 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
+import Router from 'vue-router'
+Vue.use(Router)
 //定义一个方法
 var loadTemplate = function(name) {
 	return document.getElementById(name + '_tmpl').innerHTML
 }
 //定义组件
-const Home = { template: '<img src="assets/img/home.png" alt="" width="100%">'}
-const List = { template: loadTemplate('list')}
-const Item = { template: loadTemplate('item')}
+const Home = { 
+	template: '<img src="assets/img/home.png" alt="" width="100%">'
 
+}
+
+const List = {
+ template: loadTemplate('list')
+ date: function () {
+ 	// jsonp
+ 	this.$http.jsonp('http//localhost:api/music')
+ 		.then(res=>{
+ 			this.list = res.date
+ 		})
+ 	return{
+ 		list: []
+ 	}
+ }
+}
+const Item = {
+	template: loadTemplate('item')
+	date: function () {
+		return{
+			item:songs[0]
+		}
+	}
+	route :{
+		date: function (transition) {
+			var id = parseInt(transition.to.param.id)
+			if (!id) {
+				router.go('/home')
+				return
+			}
+			return{
+				item : songs.find(s=>s.id)
+			}
+		}
+	}
+}
 
 
 // 定义路由
-const routes = [
+const router = new Router({
+	routes:[
 	{ path: '/home',name:'home', component: Home},
 	{ path: '/list',name:'list', component: List},
 	{ path: '/item',name:'item', component: item},
+	//任意其他地址跳转到home
+	{ path:'/',redirect: '/home'}
 ]
-
-const router = new VueRouter({
-	routes
 })
-//任意其他地址跳转到home
-{
-	path:'*',
-	redirect: '/home'
-}
+
 const app = new Vue({
 	router
 }).$mount('#app')
+
+export default router
